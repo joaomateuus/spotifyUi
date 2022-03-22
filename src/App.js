bf31691b37ae733ddf71ee1e1c8871c97c24d670
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-03-22 17:12:57 -0300

    first components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.scss';
+import { Sidebar } from './components/Sidebar/Sidebar.jsx';
 
 function App() {
   return (
     <div className="App">
-     
+      <Sidebar /> 
     </div>
   );
 }
