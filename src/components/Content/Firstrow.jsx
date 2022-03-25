1d98c21306699688e04b16e041a3f76be548bd8e
joaomateuus <joaomateusmoraisdeamaral@gmail.com>
2022-03-25 14:30:21 -0300

    structuring main layout

--- src/components/Content/Firstrow.jsx
+++ src/components/Content/Firstrow.jsx
@@ -6,6 +6,14 @@ export const Firstrow = () => {
     <div className='row-wrapper'>
       <div className='row'>
         <h1>Unique yours</h1>
+        <div className='cards'>
+              <span className='cards-content'>
+                <img src="https://dailymix-images.scdn.co/v2/img/ab6761610000e5ebe78b3d6a556e3dfe4341e3a5/2/pt/large" alt="" />
+                <div className='title'>
+                  Daily Mix
+                </div>
+              </span>
+        </div>
       </div>
     </div>
   )
